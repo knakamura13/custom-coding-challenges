e0791103e3653fd000dcf4b11739a49d3ce0cfa0
Kyle Nakamura <knakamura13dev@gmail.com>
2023-08-13 19:14:33 -0700

    Refactor 1.2 solution/comments; add 1 variant to 1.2

--- cracking_the_coding_interview/ctci_1.2/solution.py
+++ cracking_the_coding_interview/ctci_1.2/solution.py
@@ -1,21 +1,34 @@
 class Solution:
     def __init__(self, _input):
-        self.word_a, self.word_b = _input.split('\n')
+        self.s1, self.s2 = _input.split('\n')
 
     def solve_problem(self):
-        """Solution: O(n?) time, O(n?) space."""
-        if len(self.word_a) != len(self.word_b):
+        """Solution: O(n) time, O(n) space."""
+        if len(self.s1) != len(self.s2):
             return False
 
-        if set(self.word_a) == set(self.word_b):
+        if set(self.s1) == set(self.s2):
             return True
 
         char_counts_a, char_counts_b = {}, {}
 
-        for char in self.word_a:
-            char_counts_a[char] = 0 if char not in char_counts_a.keys() else char_counts_a[char] + 1
+        for char in self.s1:
+            char_counts_a[char] = char_counts_a.get(char, 0) + 1
 
-        for char in self.word_b:
-            char_counts_b[char] = 0 if char not in char_counts_b.keys() else char_counts_b[char] + 1
+        for char in self.s2:
+            char_counts_b[char] = char_counts_b.get(char, 0) + 1
 
         return char_counts_a == char_counts_b
+
+    def _variant_optimal_space(self):
+        """Variant: O(n) time, O(1) space."""
+        if len(self.s1) != len(self.s2):
+            return False
+        char_counts = [0] * 128  # Assuming ASCII character set
+        for char in self.s1:
+            char_counts[ord(char)] += 1
+        for char in self.s2:
+            char_counts[ord(char)] -= 1
+            if char_counts[ord(char)] < 0:
+                return False
+        return True
