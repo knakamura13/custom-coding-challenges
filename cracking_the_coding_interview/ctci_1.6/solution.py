5f17f42a1d6a2167432549cf814931d7d58f8448
Kyle Nakamura <knakamura13dev@gmail.com>
2024-07-04 04:20:33 -0700

    Add optimal space variant to ctci_1.6

--- cracking_the_coding_interview/ctci_1.6/solution.py
+++ cracking_the_coding_interview/ctci_1.6/solution.py
@@ -28,3 +28,42 @@ class Solution:
             return self.original_str
 
         return compressed_str
+
+    def _variant_optimal_space(self):
+        """Solution: O(n) time, O(1) space."""
+        if len(self.original_str) <= 2:
+            return self.original_str
+
+        # Convert the original string to a list for mutable operations
+        chars = list(self.original_str)
+        write_index = 0
+        consecutive_chars_count = 1
+
+        for i in range(1, len(chars)):
+            curr_char, prev_char = chars[i], chars[i - 1]
+
+            if curr_char == prev_char:
+                consecutive_chars_count += 1
+            else:
+                # Write the previous character and its count
+                chars[write_index] = prev_char
+                write_index += 1
+                for digit in str(consecutive_chars_count):
+                    chars[write_index] = digit
+                    write_index += 1
+                consecutive_chars_count = 1
+
+            if i == len(chars) - 1:
+                # Write the last character and its count
+                chars[write_index] = curr_char
+                write_index += 1
+                for digit in str(consecutive_chars_count):
+                    chars[write_index] = digit
+                    write_index += 1
+
+        compressed_str = ''.join(chars[:write_index])
+
+        if len(compressed_str) >= len(self.original_str):
+            return self.original_str
+
+        return compressed_str
