6dc230dd4e3c63866649a2d444c0bfc48cbe34c8
Kyle Nakamura <knakamura13dev@gmail.com>
2024-07-07 20:20:30 -0700

    Finish VERY HARD custom challenge 002 (Connectivity of Pipes)

--- custom_challenges/custom_challenge_002/solution.py
+++ custom_challenges/custom_challenge_002/solution.py
@@ -4,18 +4,45 @@ import utils
 class Solution:
     def __init__(self, _input):
         self.input = _input
-        n = len(self.input)
-
-        # Construct the 2D matrix from the input list containing chars and their coordinates
-        self.grid = [[' ' for _ in range(n)] for _ in range(n)]
-        for char, (x, y) in self.input:
-            self.grid[y][x] = char
-
-        utils.visualize_matrix(self.grid)
 
     def solve_problem(self):
         """Solution: O(n * m) time, O(n * m) space."""
-        # Temp: return all the sinks even if they aren't connected to the source
-        connected_sinks = [char[0] if 65 <= ord(char[0]) <= 90 else '' for char, coord in self.input]
+        n = len(self.input)
+        source_coord = 0, 0
+        connected_sinks = set()
+
+        # Construct a 2D matrix from the input list containing chars and their coordinates
+        grid = [[' ' for _ in range(n)] for _ in range(n)]
+        for char, (x, y) in self.input:  # Note: input coords list x before y
+            grid[y][x] = char
+            if utils.is_char_source(char):
+                source_coord = y, x
+
+        # Perform BFS on the grid, starting at the source
+        # Any sinks that can be reached using BFS must be connected to the source, so append them to connected_sinks
+        queue = [source_coord]
+        visited = set()
+        while queue:
+            curr_coord = queue.pop(0)
+
+            if curr_coord in visited:
+                continue
+
+            visited.add(curr_coord)
+            char = grid[curr_coord[0]][curr_coord[1]]
+
+            if utils.is_char_sink(char):
+                connected_sinks.add(char)
+
+            directions = utils.get_cell_opening_directions_for_char(char)
+            for direction in directions:
+                next_coord = utils.get_next_coord_in_direction(curr_coord, direction)
+                next_y, next_x = next_coord
+                if (next_coord != curr_coord
+                        and next_coord not in visited
+                        and 0 <= next_y < n
+                        and 0 <= next_x < n
+                        and grid[next_y][next_x] != ' '):
+                    queue.append(next_coord)
 
         return ''.join(sorted(connected_sinks))
--- custom_challenges/custom_challenge_002/utils.py
+++ custom_challenges/custom_challenge_002/utils.py
@@ -1,4 +1,66 @@
-def visualize_matrix(grid):
+pipe_shapes = '═', '║', '╔', '╗', '╚', ' ╝', '╠', '╣', '╦', '╩'
+
+
+def visualize_grid(grid: list[list[str]]):
     print()
     for row in grid:
         print(''.join(row))
+
+
+def is_char_source(char: str) -> bool:
+    """Returns True if the character is a valid source (i.e., asterisk character *)."""
+    return char == '*'
+
+
+def is_char_sink(char: str) -> bool:
+    """Returns True if the character is a valid sink (i.e., characters [A-Z])."""
+    return 65 <= ord(char[0]) <= 90
+
+
+def is_char_pipe(char: str) -> bool:
+    """Returns True if the character is a valid pipe shape."""
+    return char in pipe_shapes
+
+
+def get_cell_opening_directions_for_char(char: str) -> list[str]:
+    """Returns all the pipe opening directions for a given character (i.e., 'up, 'right', 'down', and/or 'left')."""
+    if is_char_source(char) or is_char_sink(char):
+        return ['up', 'right', 'down', 'left']
+
+    if char == '═':
+        return ['right', 'left']
+    elif char == '║':
+        return ['up', 'down']
+    elif char == '╔':
+        return ['right', 'down']
+    elif char == '╗':
+        return ['down', 'left']
+    elif char == '╚':
+        return ['up', 'right']
+    elif char == '╝':
+        return ['up', 'left']
+    elif char == '╠':
+        return ['up', 'right', 'down']
+    elif char == '╣':
+        return ['up', 'down', 'left']
+    elif char == '╦':
+        return ['right', 'down', 'left']
+    elif char == '╩':
+        return ['up', 'right', 'left']
+
+    return []
+
+
+def get_next_coord_in_direction(coord: tuple[int, int], direction: str) -> tuple[int, int]:
+    y, x = coord
+
+    if direction == 'up':
+        return y - 1, x
+    elif direction == 'right':
+        return y, x + 1
+    elif direction == 'down':
+        return y + 1, x
+    elif direction == 'left':
+        return y, x - 1
+
+    return coord
