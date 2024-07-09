91d8afa854cc3ed3d1ef1be85bd87290eb2fec7c
Kyle Nakamura <knakamura13dev@gmail.com>
2024-07-08 19:04:59 -0700

    Add optimal space variant to cc_003

--- custom_challenges/cc_003/solution.py
+++ custom_challenges/cc_003/solution.py
@@ -14,3 +14,25 @@ class Solution:
                 prev_smallest = num
 
         return prev_smallest + 1 if prev_smallest > 0 else 1
+
+    def _variant_optimal_space(self):
+        """Solution: O(n) time, O(1) space."""
+        n = len(self.input)
+
+        # Step 1: Mark elements out of range and filter out non-positive numbers
+        for i in range(n):
+            if self.input[i] <= 0 or self.input[i] > n:
+                self.input[i] = n + 1
+
+        # Step 2: Use the index as a hash to mark the presence of numbers
+        for i in range(n):
+            num = abs(self.input[i])
+            if num <= n:
+                self.input[num - 1] = -abs(self.input[num - 1])
+
+        # Step 3: Find the first index which is not marked
+        for i in range(n):
+            if self.input[i] > 0:
+                return i + 1
+
+        return n + 1
